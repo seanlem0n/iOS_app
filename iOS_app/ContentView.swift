a5f3a99449c97cd785de54c2f339f740f7da18a3
Sean Lim <sealim@deloitte.com>
2020-03-17 13:58:15 +0800

    Initial Commit

---
+++ iOS_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_app
+//
+//  Created by Lim, Sean Jericson Delas Alas on 17/03/2020.
+//  Copyright © 2020 deloitte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
